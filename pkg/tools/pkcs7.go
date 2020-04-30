0ae1ab5467924d82f8e6dc1cad76fa758539c4eb
Lorenzo <6698585+lorepirri@users.noreply.github.com>
2020-04-30 14:18:30 +0200

    fix padding on the Pkcs7Pad is input is blocksize
    Co-authored-by: Mariia <mashayudi@gmail.com>
    Co-authored-by: ozan kececioglu <ozankececioglu@gmail.com>

--- pkg/tools/pkcs7.go
+++ pkg/tools/pkcs7.go
@@ -2,6 +2,9 @@ package tools
 
 func Pkcs7Pad(in []byte, bs int) []byte {
 	padBytes := bs - len(in)%bs
+	if padBytes == bs {
+		padBytes = 0
+	}
 	out := make([]byte, len(in)+padBytes)
 	copy(out, in)
 	for i := 0; i < padBytes; i++ {
