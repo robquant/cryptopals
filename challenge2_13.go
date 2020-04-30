eb27df86fc54104bba09bd092d0181691f0a9e8e
Lorenzo <6698585+lorepirri@users.noreply.github.com>
2020-04-30 14:19:52 +0200

    draft, comments on the challenge

--- challenge2_13.go
+++ challenge2_13.go
@@ -1,9 +1,11 @@
 package main
 
-import ( "strings"
-		 "fmt"
-		 "math/rand"
-		 "github.com/robquant/cryptopals/pkg/tools"
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+
+	"github.com/robquant/cryptopals/pkg/tools"
 )
 
 var key []byte
@@ -27,11 +29,10 @@ func encode(object map[string]string) string {
 	params := make([]string, 0)
 	keys := []string{"email", "uid", "role"}
 	for _, key := range keys {
-		params = append(params, key + "=" + object[key])
+		params = append(params, key+"="+object[key])
 	}
 	result := strings.Join(params, "&")
 	return result
-
 }
 
 func profile_for(login string) string {
@@ -40,15 +41,15 @@ func profile_for(login string) string {
 	escaped_login := strings.ReplaceAll(login, "&", "")
 	escaped_login = strings.ReplaceAll(escaped_login, "=", "")
 
-	object := map[string]string {
+	object := map[string]string{
 		"email": escaped_login,
-		"uid": "10",
-		"role": "user",
+		"uid":   "10",
+		"role":  "user",
 	}
 	return encode(object)
 }
 
-func decode(input string) map[string] string {
+func decode(input string) map[string]string {
 	object := make(map[string]string)
 	parts := strings.Split(input, "&")
 	for _, part := range parts {
@@ -59,37 +60,106 @@ func decode(input string) map[string] string {
 	return object
 }
 
-func main(){
+func main() {
 
 	// {
 	// 	  email: 'foo@bar.com',
-	// 	  uid: 10,	
+	// 	  uid: 10,
 	// 	  role: 'user'
 	//  }
 	//profile_for("foo@bar.com")
-	input := "foo=bar&baz=qux&zap=zazzle"
-	object := decode(input)
-	fmt.Printf("%v\n", object)
-	fmt.Printf("%s\n", profile_for("foo@bar.com&role=admin"))
-	encrypted := encrypt(profile_for("foo@bar.com"), key)
-	fmt.Printf("%v\n", encrypted)
-	fmt.Printf("%s\n", decrypt(encrypted, key))
+	// input := "foo=bar&baz=qux&zap=zazzle"
+	// object := decode(input)
+	// fmt.Printf("%v\n", object)
+	// fmt.Printf("%s\n", profile_for("foo@bar.com&role=admin"))
+	// encrypted := encrypt(profile_for("foo@bar.com"), key)
+	// fmt.Printf("%v\n", encrypted)
+	// fmt.Printf("%s\n", decrypt(encrypted, key))
 
-	fmt.Printf("%v\n", encrypt(profile_for("eXail=foo@bar.com"), key))
-	fmt.Printf("%v\n", encrypt(profile_for("email=foo@bar.com"), key))
+	// fmt.Printf("%v\n", encrypt(profile_for("foX@bar.com"), key))
+	// fmt.Printf("%v\n", encrypt(profile_for("foo@bar.com"), key))
 
 	// the bloc is 16 then, because the first 16 are different
-	// [218 29 26 197 57 171 166 150 156  84   8 125 183 128  60 209 203 66 101 159 166 117 53 210 85 122 244 7 117 130 203 176 164 136 52 
-	// 232 115 5 77 185 232 37 123 52 217 114 241 52]
 
-	// [ 30 12 97 215 74  90 243 119  16 145 103  94 208  78 189  46 203 66 101 159 166 117 53 210 85 122 244 7 117 130 203 176 164 136 52 23
-	// 2 115 5 77 185 232 37 123 52 217 114 241 52]
+	// [213 223 241  63 213  58 116  70 231 188 223 83    7 238  68  20
+	//  126 186 136 160 192  75   1 112 145   1  82 198 220 172 102 113
+	//   62  44 183 210 239 182 246  55 233 240  22 134 239 207  21  56]
+
+	// [207 203  67 117 178 156 149 253 189 46 255 193 148 151 39
+	//  191 126 186 136 160 192 75 1 112 145 1 82 198 22 0 172 102 113 62 44 183 210 239 182 246 55 233 240 22 134 239 207 21 56]
 
 	// email=foo@bar.com&uid=10&role=user
 
 	// email=12345678999999999999999@usern.com&uid=10&role=user
 
-	// email=12345678999999999999999@admin.com&uid=10&role=user
+	// email=123456789@admin.com&uid=10&role=user
 
 	// email=foo@bar.com&uid=10&role=user
+
+	// email=foo@bar.com&uid=10&role=user&role=admin
+
+	// &role=admin it will 16 bytes, a block
+
+	// email=foo@bar.com&uid=10&role=user 3 blocks
+
+	// 1. keep changing the email until this goes to the last block:
+	// &role=user
+
+	// 2. we need to have this encrypted as last block:
+	// &role=admin
+
+	// 3. we will change just the last block with the admin one.
+
+	fmt.Printf("% X\n", encrypt(profile_for("A23456789@admin.com"), key))
+
+	// email=123456789@
+	// admin.com&uid=10
+	// &role=user......
+	// C2 CD D0 02 13 56 59 3A D4 71 8F 06 F9 2B 33 99
+	// 60 A6 29 BD 9E B9 CB 0B E1 B7 FD 5B 5A C2 D5 EF
+	// A6 F0 8B 2B 57 9C E8 0B 48 C0 45 F5 7E 98 14 7F
+
+	// CF 3B BC 43 C8 67 1A F0 65 E6 ED B5 F2 95 D6 AC
+	// 60 A6 29 BD 9E B9 CB 0B E1 B7 FD 5B 5A C2 D5 EF
+	// A6 F0 8B 2B 57 9C E8 0B 48 C0 45 F5 7E 98 14 7F
+
+	// 1.
+	// email=123456789@
+	// c.m&uid=10&role=
+	// user.............
+
+	// 2. 123456789@admin           c.m
+	// email=123456789@
+	// admin
+	// adminXXXXXXXXXXX
+	// c.m&uid=10&role=
+	// user.............
+
+	// admin............
+	//adminXXXXXXXXXXX
+
+	// C2 CD D0 02 13 56 59 3A D4 71 8F 06 F9 2B 33 99
+	// 60 A6 29 BD 9E B9 CB 0B E1 B7 FD 5B 5A C2 D5 EF
+	// A6 F0 8B 2B 57 9C E8 0B 48 C0 45 F5 7E 98 14 7F
+
+	// CF 3B BC 43 C8 67 1A F0 65 E6 ED B5 F2 95 D6 AC
+	// 60 A6 29 BD 9E B9 CB 0B E1 B7 FD 5B 5A C2 D5 EF
+	// A6 F0 8B 2B 57 9C E8 0B 48 C0 45 F5 7E 98 14 7F
+
+	// email=123456789@
+	// adminXXXXXXXXXXX
+	// &role=user......
+	// C2 CD D0 02 13 56 59 3A D4 71 8F 06 F9 2B 33 99
+	// 60 A6 29 BD 9E B9 CB 0B E1 B7 FD 5B 5A C2 D5 EF
+	// A6 F0 8B 2B 57 9C E8 0B 48 C0 45 F5 7E 98 14 7F
+
+	// CF 3B BC 43 C8 67 1A F0 65 E6 ED B5 F2 95 D6 AC
+	// 60 A6 29 BD 9E B9 CB 0B E1 B7 FD 5B 5A C2 D5 EF
+	// A6 F0 8B 2B 57 9C E8 0B 48 C0 45 F5 7E 98 14 7F
+
+	// "email=foo@bar.com&uid=10&role=admin           "
+
+	// "role=admin\0x00           "
+	// k="role"
+	// v="admin           "
 }
