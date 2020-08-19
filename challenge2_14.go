e477dc4feaa6ac1b134f41aa5d5fcf8a3f37ed31
Robert Franke <robert.franke@nirx.de>
2020-08-19 14:13:16 +0200

    Finally 2/14 working!

--- challenge2_14.go
+++ challenge2_14.go
@@ -5,7 +5,9 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
+	"math"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/robquant/cryptopals/pkg/tools"
@@ -18,10 +20,12 @@ var plaintext []byte
 var randomPrefix []byte
 
 func init() {
+	rand.Seed(time.Now().UnixNano())
 	key = make([]byte, 16)
 	rand.Read(key)
 	prefixLen := rand.Intn(100)
 	randomPrefix = make([]byte, prefixLen)
+	fmt.Printf("Generated random prefix of len %d\n", prefixLen)
 	rand.Read(randomPrefix)
 }
 
@@ -35,17 +39,17 @@ func oracle(prefix []byte) []byte {
 	return tools.EncryptAesECB(combined, key)
 }
 
-//
-func crackLastLetterInBlock(prefix, target []byte, blockSize int) byte {
-	attackerString := make([]byte, blockSize)
-	copy(attackerString, prefix)
+func crackLastLetterInBlock(blockOffset, padding int, prefix, target []byte, blockSize int) byte {
+	attackerString := bytes.Repeat([]byte{65}, padding)
+	attackerString = append(attackerString, prefix...)
+	attackerString = append(attackerString, 0)
 	// We know that target == oracle(prefix + unknown letter)
 	// We call oracle with all possible letters in the last position and compare
 	// to the target block. If they are identical we found the letter.
 	for b := 0; b <= 255; b++ {
-		attackerString[blockSize-1] = byte(b)
+		attackerString[len(attackerString)-1] = byte(b)
 		enrypted := oracle(attackerString)
-		if bytes.Compare(enrypted[:blockSize], target) == 0 {
+		if bytes.Compare(enrypted[blockOffset*blockSize:(blockOffset+1)*blockSize], target) == 0 {
 			return byte(b)
 		}
 	}
@@ -60,33 +64,65 @@ func main() {
 		log.Fatal(err)
 	}
 	// Figure out the block size of the encryption algorithm
-	// by feeding it 2 blocksize blocks of repeated "A"'s and look for
+	// by feeding it 3 blocksize blocks of repeated "A"'s and look for
 	// repetitions in the encrypted output
-	var blockSize int
-	for bs := 1; bs <= 33; bs++ {
-		prefix := bytes.Repeat([]byte{65}, 2*bs)
+	var blockSize int = 0
+	for bs := 16; bs <= 33; bs++ {
+		prefix := bytes.Repeat([]byte{65}, 3*bs)
 		encrypted := oracle(prefix)
-		if bytes.Compare(encrypted[:bs], encrypted[bs:2*bs]) == 0 {
-			blockSize = bs
+		nblocks := int(math.Floor(float64(len(encrypted))/float64(bs))) - 1
+		for iblock := 0; iblock < nblocks; iblock++ {
+			if bytes.Compare(encrypted[bs*iblock:bs*(iblock+1)], encrypted[bs*(iblock+1):bs*(iblock+2)]) == 0 {
+				blockSize = bs
+				break
+			}
+		}
+		if blockSize > 0 {
 			break
 		}
 	}
 	fmt.Printf("Blocksize is %d\n", blockSize)
+	if blockSize == 0 {
+		fmt.Printf("fail\n")
+		os.Exit(1)
+	}
+
+	prefix := bytes.Repeat([]byte{65}, blockSize*2)
+	var blockStartFound int = -1
+	for i := 0; i < blockSize; i++ {
+		encrypted := oracle(prefix)
+		nblocks := int(math.Floor(float64(len(encrypted))/float64(blockSize))) - 1
+		for iblock := 0; iblock < nblocks; iblock++ {
+			if bytes.Compare(encrypted[blockSize*iblock:blockSize*(iblock+1)], encrypted[blockSize*(iblock+1):blockSize*(iblock+2)]) == 0 {
+				blockStartFound = iblock
+				break
+			}
+		}
+		if blockStartFound >= 0 {
+			break
+		}
+		prefix = append(prefix, 65)
+	}
+	randomPrefixLen := blockSize*(blockStartFound+2) - len(prefix)
+	fmt.Printf("randomPrefix: %d\n", randomPrefixLen)
+
+	padding := blockSize - randomPrefixLen%blockSize
 	decoded := bytes.Repeat([]byte{65}, blockSize)
+	blockOffset := (padding + randomPrefixLen) / blockSize
 	for i := 0; i < len(plaintext); i++ {
-		blockIndex := i / blockSize // block of plaintext we are cracking
+		blockIndex := blockOffset + i/blockSize // block of plaintext we are cracking
 		// We alway make sure the next letter the decode is last in a block
 		// and we hae decoded all the letters before this one
 		// E.g. to decode the first plaintext letters we prefix with
 		// blocksize - 1 "A"s
 		prefixLength := blockSize - (i % blockSize) - 1 //
-		prefix := bytes.Repeat([]byte{65}, prefixLength)
+		prefix := bytes.Repeat([]byte{65}, padding+prefixLength)
 		encrypted := oracle(prefix)
 		// Cut out the block were the letter we are trying to crack is last in the block
 		targetBlock := encrypted[blockIndex*blockSize : (blockIndex+1)*blockSize]
 		// decoded[len(decoded)-blockSize+1:] are the letters before the target letter that
 		// we have already decoded (or chosen)
-		b := crackLastLetterInBlock(decoded[len(decoded)-blockSize+1:], targetBlock, blockSize)
+		b := crackLastLetterInBlock(blockOffset, padding, decoded[len(decoded)-blockSize+1:], targetBlock, blockSize)
 		decoded = append(decoded, b)
 	}
 	fmt.Println(string(decoded[blockSize:]))
